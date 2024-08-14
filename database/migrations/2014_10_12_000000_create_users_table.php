c531153871cf77cefa22c16426abc330aa92623b
christi67 <122974627+christi67@users.noreply.github.com>
2024-08-14 13:21:08 +0300

    Job table and user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,14 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string( 'First_name', 50);
+            $table->string( 'last_name', 50);
+            $table->string( 'username', 20);
+            $table->integer( 'Age');
+            $table->enum( 'Gender', ['male', 'female']);
+            $table->string( 'phone_number', 15);
+            $table->string( 'Adress');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -22,9 +26,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('users');
---
+++ database/migrations/2024_08_14_101401_create_job_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('job_name');
+            $table->enum('job_field', [
+                'Computer Science', 
+                'Engineering', 
+                'Health', 
+                'Architecture', 
+                'Business', 
+                'Education',
+                'Art',
+                'Law',
+                'Science',
+                'Finance'
+            ]);
+            $table->string('job_company');
+            $table->string('job_adress');
+            $table->enum( 'job_type', ['remote', 'in_person']);
+            $table->string('job_salary');
+            $table->date('job_deadline');
+            $table->string('job_required_qualification');
+
+
+            $table->timestamps();
+        });
+    }
+
+   
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
